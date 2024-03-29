3776056906128a491c690cc0840babd42dc30857
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-03-30 03:08:54 +0400

    added getter fucntions for name and nationality

--- class 2.py
+++ class 2.py
@@ -2,4 +2,18 @@
 #class for artist who creates artworks
 class Artist:
     #in def for Initializing an Artist object with a name and nationality.
-    def __init__(self, name, nationality):
+    def __init__(self, name: str, nationality: str):
+        self.name= name
+        self.nationality= nationality
+        self.artworks= []
+
+    def get_name(self) -> str:
+        return self.name
+    def get_nationality(self) -> str:
+        return self.nationality
+
+
+
+
+
+
