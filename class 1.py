656e8385ece634861c1be1c2e21a3859ff3ef609
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-03-29 20:38:59 +0400

    create class for Art, def function for attributes to initialize

--- class 1.py
+++ class 1.py
@@ -1,3 +1,10 @@
 #first class from uml; Art
 class Art:
-    
+    #constructor method for initializing :
+    def __init__(self, title, artist, history_significance, dateproduced):
+        self.title= title #initializing title attribute with "title" paremeter
+        self.artist= artist
+        self.history_significance= history_significance
+        self.dateproduced= dateproduced
+
+
