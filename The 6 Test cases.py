c5e3bc17c745783edcde25a0297831026234c565
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-03-30 15:48:06 +0400

    creating new artwork objects with filled paremeters

--- The 6 Test cases.py
+++ The 6 Test cases.py
@@ -1,1 +1,17 @@
-#a
+#First test case
+
+def test_add_art():
+    artist = Artist("Leonardo da Vinci", "Italian") #artist object
+
+#Creating new artworks to test
+    artwork1 = Art("Mona Lisa", "Leonardo da Vinci", "An iconic portrait of Lisa Gherardini", "c. 1503")
+    artwork2 = Art("The Last Supper", "Leonardo da Vinci", "A mural painting depicting the Last Supper of Jesus", "1495–1498")
+
+
+
+
+
+
+
+
+
