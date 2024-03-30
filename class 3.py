3786b57465a345eee09aad28d4f225929abd319d
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-03-30 05:29:17 +0400

    added comments

--- class 3.py
+++ class 3.py
@@ -1,11 +1,14 @@
 #class 3 for Ticket
 
 class Ticket:
+    #initializing the ticket object with the attributes
     def __init__(self, ticket_num, event: str, price: float, is_group: bool):
         self.ticket_num=ticket_num
         self.event = event
         self.price = price
-        self.is_group = is_group
+        self.is_group = is_group #looks at if the ticket is for a group or individual
 
 
-    #g
+    #getter functions for retrieving attributes values
+
+    def
