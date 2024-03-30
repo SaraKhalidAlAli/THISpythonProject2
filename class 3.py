65200171d34a5f91a68f772e04ffb6144cfbab06
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-03-30 05:26:14 +0400

    added this part of the code provides a clear interface for accessing and updating Ticket objects' attributes.

--- class 3.py
+++ class 3.py
@@ -1,1 +1,11 @@
 #class 3 for Ticket
+
+class Ticket:
+    def __init__(self, ticket_num, event: str, price: float, is_group: bool):
+        self.ticket_num=ticket_num
+        self.event = event
+        self.price = price
+        self.is_group = is_group
+
+
+    #g
