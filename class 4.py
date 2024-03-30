1f6f1ebc09a96881dc6a4cc89bdd0dc667b7b268
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-03-30 06:21:26 +0400

    added return to exit a function and return a value.

--- class 4.py
+++ class 4.py
@@ -8,7 +8,10 @@ class Customer:
         self.age= age
 
     def get_id(self) -> str:
+        return self.id
 
     def get_email(self) -> str:
+        return self.email
 
     def get_age(self) -> int:
+        return self.age
