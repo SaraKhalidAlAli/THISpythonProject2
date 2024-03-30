fb328961d00d96bace0d7920e3a2f70a429e56cc
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-03-30 15:51:20 +0400

    adding the artworks to the museum here

--- The 6 Test cases.py
+++ The 6 Test cases.py
@@ -4,9 +4,12 @@ def test_add_art():
     artist = Artist("Leonardo da Vinci", "Italian") #artist object
 
 #Creating new artworks to test
-    artwork1 = Art("Mona Lisa", "Leonardo da Vinci", "An iconic portrait of Lisa Gherardini", "c. 1503")
-    artwork2 = Art("The Last Supper", "Leonardo da Vinci", "A mural painting depicting the Last Supper of Jesus", "1495–1498")
+    artwork1 =Art("Mona Lisa", "Leonardo da Vinci", "An iconic portrait of Lisa Gherardini", "c. 1503")
+    artwork2 =Art("The Last Supper", "Leonardo da Vinci", "A mural painting depicting the Last Supper of Jesus", "1495–1498")
 
+  #adding the artworks to the museum here
+    artist.add_artwork(artwork1)
+    artist.add_artwork(artwork2)
 
 
 
