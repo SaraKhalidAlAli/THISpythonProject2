f94b2adb80dfd9574a24181a65b6d71438d5a446
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-03-30 15:23:31 +0400

    finishing the initializer sections and adding comments

--- class 6.py
+++ class 6.py
@@ -2,7 +2,10 @@
 
 class EventWebsite:
     def __init__(self, event_name:str, event_description:str, url:str, location:str):
-        self.
-        self.
-        self.
-        self.
+        self.event_name = event_name  #storing the name of the event
+        self.event_description = event_description  #storing the description of the event
+        self.url = url  #storing the URL of the event
+        self.location = location  #storing the location of the event
+
+
+
