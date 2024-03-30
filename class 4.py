525bf1d4db63af8e3d3e99a5e563f71de288b02a
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-03-30 06:36:15 +0400

    added comments to the getter functions section

--- class 4.py
+++ class 4.py
@@ -7,11 +7,13 @@ class Customer:
         self.email = email
         self.age= age
 
+#Getter functions
     def get_id(self) -> str:
         return self.id
-
+        # Return the customer ID
     def get_email(self) -> str:
         return self.email
-
+        # Return the customer email
     def get_age(self) -> int:
         return self.age
+        # Return the customer age
