366037facbb9c620d833e41b36bf77ffe4906e26
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-03-30 07:00:49 +0400

    adding the method for the attributes name and role

--- Class 5.py
+++ Class 5.py
@@ -1,3 +1,10 @@
 # class 5; Employee
-
+'''This class represents an employee with attributes for name and role, along with getter and setter methods for accessing and modifying these attributes.
+'''
 class Employee:
+
+#useing __init__ method that initializes an Employee object with the provided name and role.
+    def __init__(self, name: str, role: str):
+        self.name = name
+        self.role = role
+
