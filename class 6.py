f3c15875f86d123644df838e446322ea072d41da
SaraKhalidAlAli <202206310@zu.ac.ae>
2024-03-30 15:17:13 +0400

    self

--- class 6.py
+++ class 6.py
@@ -1,4 +1,8 @@
 #class 6; EventWebsite
 
 class EventWebsite:
-    def __init__(self,):
+    def __init__(self, event_name:str, event_description:str, url:str, location:str):
+        self.
+        self.
+        self.
+        self.
